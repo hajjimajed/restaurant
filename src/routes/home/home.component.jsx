da77f4168c8c951e300b8cdf15f13e82f119603f
hajjimajed <hajjimajed78@gmail.com>
2023-06-05 15:42:53 +0100

    finishing navbar and two first sections

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,228 @@
+import './home.styles.scss'
+
+
+const Home = () => {
+
+    return (
+        <div className='home-container'>
+            <div class="container-xxl py-5 bg-dark hero-header mb-5">
+                <div class="container  py-5">
+                    <div class="row align-items-center g-5">
+                        <div class="col-lg-6 text-center text-lg-start">
+                            <h1 class="display-3 text-white animated slideInLeft">Enjoy Our <br /> Delicious Meal</h1>
+                            <p class="text-white animated slideInLeft mb-4 pb-2">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
+                            <a href="" class="btn btn-primary py-sm-3 px-sm-5 me-3 animated slideInLeft">Book A Table</a>
+                        </div>
+                        <div class="col-lg-6 text-center text-lg-end overflow-hidden">
+                            <img class="img-fluid spin-animation" src="https://i.ibb.co/Mkzz865/hero.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container-xxl py-5">
+                <div class="container">
+                    <div class="row g-4">
+                        <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+                            <div class="service-item rounded pt-3">
+                                <div class="p-4">
+                                    <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                                        width="100.000000pt" height="100.000000pt" viewBox="0 0 100.000000 100.000000"
+                                        preserveAspectRatio="xMidYMid meet">
+
+                                        <g transform="translate(0.000000,100.000000) scale(0.100000,-0.100000)"
+                                            fill="#000000" stroke="none">
+                                            <path d="M405 967 c-123 -41 -214 -130 -249 -244 -6 -19 -3 -23 13 -23 14 0
+26 15 44 53 30 66 81 118 150 154 45 24 64 28 137 28 69 0 94 -5 132 -24 66
+-32 125 -93 155 -157 18 -39 30 -54 44 -54 16 0 19 4 13 23 -44 143 -178 246
+-328 254 -41 1 -88 -3 -111 -10z"/>
+                                            <path d="M120 640 c-18 -18 -20 -33 -20 -190 0 -157 2 -172 20 -190 25 -25 55
+-25 80 0 18 18 20 33 20 190 0 157 -2 172 -20 190 -11 11 -29 20 -40 20 -11 0
+-29 -9 -40 -20z"/>
+                                            <path d="M800 640 c-18 -18 -20 -33 -20 -190 0 -157 2 -172 20 -190 25 -25 55
+-25 80 0 18 18 20 33 20 190 0 157 -2 172 -20 190 -11 11 -29 20 -40 20 -11 0
+-29 -9 -40 -20z"/>
+                                            <path d="M41 577 c-42 -51 -52 -147 -22 -213 35 -77 41 -65 41 85 0 78 -2 141
+-4 141 -2 0 -9 -6 -15 -13z"/>
+                                            <path d="M940 450 c0 -151 6 -163 41 -86 10 22 19 61 19 86 0 25 -9 64 -19 86
+-35 77 -41 65 -41 -86z"/>
+                                            <path d="M795 174 c-18 -28 -70 -54 -110 -54 -18 0 -25 -5 -25 -20 0 -48 134
+-7 170 52 25 41 25 48 1 48 -10 0 -26 -12 -36 -26z"/>
+                                            <path d="M420 140 c-11 -11 -20 -29 -20 -40 0 -41 34 -60 110 -60 76 0 110 19
+110 60 0 11 -9 29 -20 40 -29 29 -151 29 -180 0z"/>
+                                        </g>
+                                    </svg>
+                                    <h5>Master Chefs</h5>
+                                    <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="service-item rounded pt-3">
+                                <div class="p-4">
+                                    <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                                        width="100.000000pt" height="100.000000pt" viewBox="0 0 100.000000 100.000000"
+                                        preserveAspectRatio="xMidYMid meet">
+
+                                        <g transform="translate(0.000000,100.000000) scale(0.100000,-0.100000)"
+                                            fill="#000000" stroke="none">
+                                            <path d="M405 967 c-123 -41 -214 -130 -249 -244 -6 -19 -3 -23 13 -23 14 0
+26 15 44 53 30 66 81 118 150 154 45 24 64 28 137 28 69 0 94 -5 132 -24 66
+-32 125 -93 155 -157 18 -39 30 -54 44 -54 16 0 19 4 13 23 -44 143 -178 246
+-328 254 -41 1 -88 -3 -111 -10z"/>
+                                            <path d="M120 640 c-18 -18 -20 -33 -20 -190 0 -157 2 -172 20 -190 25 -25 55
+-25 80 0 18 18 20 33 20 190 0 157 -2 172 -20 190 -11 11 -29 20 -40 20 -11 0
+-29 -9 -40 -20z"/>
+                                            <path d="M800 640 c-18 -18 -20 -33 -20 -190 0 -157 2 -172 20 -190 25 -25 55
+-25 80 0 18 18 20 33 20 190 0 157 -2 172 -20 190 -11 11 -29 20 -40 20 -11 0
+-29 -9 -40 -20z"/>
+                                            <path d="M41 577 c-42 -51 -52 -147 -22 -213 35 -77 41 -65 41 85 0 78 -2 141
+-4 141 -2 0 -9 -6 -15 -13z"/>
+                                            <path d="M940 450 c0 -151 6 -163 41 -86 10 22 19 61 19 86 0 25 -9 64 -19 86
+-35 77 -41 65 -41 -86z"/>
+                                            <path d="M795 174 c-18 -28 -70 -54 -110 -54 -18 0 -25 -5 -25 -20 0 -48 134
+-7 170 52 25 41 25 48 1 48 -10 0 -26 -12 -36 -26z"/>
+                                            <path d="M420 140 c-11 -11 -20 -29 -20 -40 0 -41 34 -60 110 -60 76 0 110 19
+110 60 0 11 -9 29 -20 40 -29 29 -151 29 -180 0z"/>
+                                        </g>
+                                    </svg>
+                                    <h5>Quality Food</h5>
+                                    <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+                            <div class="service-item rounded pt-3">
+                                <div class="p-4">
+                                    <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                                        width="100.000000pt" height="100.000000pt" viewBox="0 0 100.000000 100.000000"
+                                        preserveAspectRatio="xMidYMid meet">
+
+                                        <g transform="translate(0.000000,100.000000) scale(0.100000,-0.100000)"
+                                            fill="#000000" stroke="none">
+                                            <path d="M405 967 c-123 -41 -214 -130 -249 -244 -6 -19 -3 -23 13 -23 14 0
+26 15 44 53 30 66 81 118 150 154 45 24 64 28 137 28 69 0 94 -5 132 -24 66
+-32 125 -93 155 -157 18 -39 30 -54 44 -54 16 0 19 4 13 23 -44 143 -178 246
+-328 254 -41 1 -88 -3 -111 -10z"/>
+                                            <path d="M120 640 c-18 -18 -20 -33 -20 -190 0 -157 2 -172 20 -190 25 -25 55
+-25 80 0 18 18 20 33 20 190 0 157 -2 172 -20 190 -11 11 -29 20 -40 20 -11 0
+-29 -9 -40 -20z"/>
+                                            <path d="M800 640 c-18 -18 -20 -33 -20 -190 0 -157 2 -172 20 -190 25 -25 55
+-25 80 0 18 18 20 33 20 190 0 157 -2 172 -20 190 -11 11 -29 20 -40 20 -11 0
+-29 -9 -40 -20z"/>
+                                            <path d="M41 577 c-42 -51 -52 -147 -22 -213 35 -77 41 -65 41 85 0 78 -2 141
+-4 141 -2 0 -9 -6 -15 -13z"/>
+                                            <path d="M940 450 c0 -151 6 -163 41 -86 10 22 19 61 19 86 0 25 -9 64 -19 86
+-35 77 -41 65 -41 -86z"/>
+                                            <path d="M795 174 c-18 -28 -70 -54 -110 -54 -18 0 -25 -5 -25 -20 0 -48 134
+-7 170 52 25 41 25 48 1 48 -10 0 -26 -12 -36 -26z"/>
+                                            <path d="M420 140 c-11 -11 -20 -29 -20 -40 0 -41 34 -60 110 -60 76 0 110 19
+110 60 0 11 -9 29 -20 40 -29 29 -151 29 -180 0z"/>
+                                        </g>
+                                    </svg>
+                                    <h5>Online Order</h5>
+                                    <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+                            <div class="service-item rounded pt-3">
+                                <div class="p-4">
+                                    <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                                        width="100.000000pt" height="100.000000pt" viewBox="0 0 100.000000 100.000000"
+                                        preserveAspectRatio="xMidYMid meet">
+
+                                        <g transform="translate(0.000000,100.000000) scale(0.100000,-0.100000)"
+                                            fill="#000000" stroke="none">
+                                            <path d="M405 967 c-123 -41 -214 -130 -249 -244 -6 -19 -3 -23 13 -23 14 0
+26 15 44 53 30 66 81 118 150 154 45 24 64 28 137 28 69 0 94 -5 132 -24 66
+-32 125 -93 155 -157 18 -39 30 -54 44 -54 16 0 19 4 13 23 -44 143 -178 246
+-328 254 -41 1 -88 -3 -111 -10z"/>
+                                            <path d="M120 640 c-18 -18 -20 -33 -20 -190 0 -157 2 -172 20 -190 25 -25 55
+-25 80 0 18 18 20 33 20 190 0 157 -2 172 -20 190 -11 11 -29 20 -40 20 -11 0
+-29 -9 -40 -20z"/>
+                                            <path d="M800 640 c-18 -18 -20 -33 -20 -190 0 -157 2 -172 20 -190 25 -25 55
+-25 80 0 18 18 20 33 20 190 0 157 -2 172 -20 190 -11 11 -29 20 -40 20 -11 0
+-29 -9 -40 -20z"/>
+                                            <path d="M41 577 c-42 -51 -52 -147 -22 -213 35 -77 41 -65 41 85 0 78 -2 141
+-4 141 -2 0 -9 -6 -15 -13z"/>
+                                            <path d="M940 450 c0 -151 6 -163 41 -86 10 22 19 61 19 86 0 25 -9 64 -19 86
+-35 77 -41 65 -41 -86z"/>
+                                            <path d="M795 174 c-18 -28 -70 -54 -110 -54 -18 0 -25 -5 -25 -20 0 -48 134
+-7 170 52 25 41 25 48 1 48 -10 0 -26 -12 -36 -26z"/>
+                                            <path d="M420 140 c-11 -11 -20 -29 -20 -40 0 -41 34 -60 110 -60 76 0 110 19
+110 60 0 11 -9 29 -20 40 -29 29 -151 29 -180 0z"/>
+                                        </g>
+                                    </svg>
+                                    <h5>24/7 Service</h5>
+                                    <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <div class="container-xxl py-5">
+                <div class="container">
+                    <div class="row g-5 align-items-center">
+                        <div class="col-lg-6">
+                            <div class="row g-3">
+                                <div class="col-6 text-start">
+                                    <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.1s" src="img/about-1.jpg" />
+                                </div>
+                                <div class="col-6 text-start">
+                                    <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.3s" src="img/about-2.jpg" />
+                                </div>
+                                <div class="col-6 text-end">
+                                    <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.5s" src="img/about-3.jpg" />
+                                </div>
+                                <div class="col-6 text-end">
+                                    <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.7s" src="img/about-4.jpg" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <h5 class="section-title ff-secondary text-start text-primary fw-normal">About Us</h5>
+                            <h1 class="mb-4">Welcome to <i class="fa fa-utensils text-primary me-2"></i>Restoran</h1>
+                            <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos erat ipsum et lorem et sit, sed stet lorem sit.</p>
+                            <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
+                            <div class="row g-4 mb-4">
+                                <div class="col-sm-6">
+                                    <div class="d-flex align-items-center border-start border-5 border-primary px-3">
+                                        <h1 class="flex-shrink-0 display-5 text-primary mb-0" data-toggle="counter-up">15</h1>
+                                        <div class="ps-4">
+                                            <p class="mb-0">Years of</p>
+                                            <h6 class="text-uppercase mb-0">Experience</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="d-flex align-items-center border-start border-5 border-primary px-3">
+                                        <h1 class="flex-shrink-0 display-5 text-primary mb-0" data-toggle="counter-up">50</h1>
+                                        <div class="ps-4">
+                                            <p class="mb-0">Popular</p>
+                                            <h6 class="text-uppercase mb-0">Master Chefs</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        </div>
+
+    )
+}
+
+export default Home;
